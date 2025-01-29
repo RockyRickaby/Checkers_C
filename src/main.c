2e90e022a288e97eb5eefef18abd571c47adf0cc
Mauricio Luz <iamthefreeman@outlook.com.br>
2025-01-29 14:12:23 -0300

    incomplete main file. just so the project can actually be compiled and ran

--- src/main.c
+++ src/main.c
@@ -6,31 +6,40 @@
 #include "game/checkers.h"
 #include "terminal_ui/terminal_ui.h"
 
-int main(/*int argc, char const *argv[]*/ void)
-{
-    terminalGameBegin();
-    // printf("Hello World!\n");
-    // struct Board board;
-    // boardInit(&board);
-    // memset(board.board, '.', (size_t) (board.boardSize * board.boardSize));
-    // // board.board[8][1] = board.pieceDarkMan;
-    // board.board[7][2] = board.pieceDarkMan;
-    // // board.board[6][3] = board.pieceDarkMan;
-    // boardPrint(&board);
-    // printf("%d\n", boardTryMoveOrCapture(&board, CHECKERS_PLAYER_TWO, (struct Point){.y = 7, .x = 2}, (struct Point){.y = 8, .x = 1}));
-    // // printf("%d\n", checkersTryMoveOrCapture(&board, CHECKERS_PLAYER_TWO, (struct Point){ .y = 3, .x = 0 }, (struct Point){ .y = 4, .x = 1 }));
-    // // printf("%d\n", checkersTryMoveOrCapture(&board, CHECKERS_PLAYER_ONE, (struct Point){ .y = 6, .x = 1 }, (struct Point){ .y = 5, .x = 2 }));
-    // // printf("%d\n", checkersTryMoveOrCapture(&board, CHECKERS_PLAYER_ONE, (struct Point){ .y = 5, .x = 2 }, (struct Point){ .y = 3, .x = 0 }));
-    // // printf("%d\n", checkersTryMoveOrCapture(&board, CHECKERS_PLAYER_ONE, (struct Point){ .y = 3, .x = 0 }, (struct Point){ .y = 4, .x = 1 }));
-    // // printf("%d\n", checkersTryMoveOrCapture(&board, CHECKERS_PLAYER_TWO, (struct Point){ .y = 3, .x = 2 }, (struct Point){ .y = 5, .x = 0 }));
-    // // struct Point piecePos = { .x = 1, .y = 6 };
-    // // struct Point moves[3] = {
-    // //     {.x = 0, .y =5 },
-    // //     {.x = 1, .y =4 },
-    // //     {.x = 2, .y =5 }
-    // // };
-    // // printf("%d\n", checkersMultiMoveCapture(&board, CHECKERS_PLAYER_ONE, piecePos, moves, 3ULL));
-    // // printf("%d\n", checkersTryMoveOrCapture(&board, CHECKERS_PLAYER_ONE, piecePos, moves[0]));
-    // boardPrint(&board);
-    return 0;
+int main(int argc, char const *argv[]) {
+    if (argc == 2 && strcmp(argv[1], "help") == 0) {
+        printf(
+            "Usage:\n"
+            "\t./main.exe\n"
+            "\t./main.exe <enable_forcecapture> <enable_ai>\tEx.: ./main 1 0\n"
+            "\t./main.exe <moves_file_path> <enable_forcecapture> <enable_ai>\t.Ex: ./main test.txt 0 1\n"
+            "\t./main.exe <use_gui> <enable_forcecapture> <enable_ai>\tEx.: ./main 1 test.txt 1 1\n"
+            "\t./main.exe <use_gui> <moves_file_path> <enable_forcecapture> <enable_ai>\tEx.: ./main 1 test.txt 1 1\n"
+        );
+        return 0;
+    }
+
+    if (argc == 3) {
+        int forcecap, aienable;
+        forcecap = atoi(argv[1]);
+        aienable = atoi(argv[2]);
+
+        struct Checkers game;
+        // checkersInit()
+    } else if (argc == 4) {
+
+    } else if (argc == 5) {
+
+    } else {
+        FILE* file = fopen("test.txt", "r");
+        if (!file) {
+            printf("fail\n");
+            return 1;
+        }
+        struct Checkers game;
+        checkersInit(&game, 1);
+        terminalCheckersBeginF(&game, stdin);
+        fclose(file);
+        return 0;
+    }
 }
